759d489d4b1c431ff7ebbd265bc2367ceaed0cae
Pyth-agoras <fsd.goken@gmail.com>
2026-07-07 19:53:58 -0400

    Update tool_install_orchestrator.js

--- src/tool_install_orchestrator.js
+++ src/tool_install_orchestrator.js
@@ -1,1 +1,10 @@
-
+else if(!valid){
+  workflow_status='validation_required';
+  next_required_tool='tool_installation_validator';
+  next_required_action='Validate the proposed installation payload.';
+}
+else{
+  workflow_status='ready_to_install';
+  next_required_tool='foundry_operator';
+  next_required_action='Install approved files and deploy.';
+}
